0ac5aa56550b68ab05af279a9835b49917d95fae
Simona Kurňavová <kurnavova.simona@gmail.com>
2018-09-15 18:02:15 +0200

    Colors and action bars

--- app/src/main/java/com/simona/budgetapp/AddRecordActivity.java
+++ app/src/main/java/com/simona/budgetapp/AddRecordActivity.java
@@ -1,46 +1,50 @@
 package com.simona.budgetapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class OverviewActivity extends AppCompatActivity {
+public class AddRecordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_overview);
+        setContentView(R.layout.activity_add_record);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setImageResource(R.drawable.ic_add);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplication(), AddRecordActivity.class);
-                startActivity(intent);
-            }
-        });
+        // Sets return button on action bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_overview, menu);
+        getMenuInflater().inflate(R.menu.menu_add_record, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.save_record) {
+            saveRecord();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void saveRecord() {
+        // TODO: save new record
+    }
+
 }
+
